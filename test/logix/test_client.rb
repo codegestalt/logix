ffd02a81450f391892f9d939c2bd56c36c5d7634
Rodrigo Haenggi <rodrigo@codegestalt.com>
2015-07-02 14:56:35 +0200

    client setup block

--- test/logix/test_client.rb
+++ test/logix/test_client.rb
@@ -2,15 +2,17 @@ require 'minitest_helper'
 
 class Logix::ClientTest < MiniTest::Test
 
+  def setup
+    @client = Logix::Client.new
+  end
+
   def test_get_user_agent_default
-    client = Logix::Client.new 
-    assert_equal "Logix/#{Logix::VERSION}", client.user_agent
+    assert_equal "Logix/#{Logix::VERSION}", @client.user_agent
   end
 
   def test_overwrite_user_agent
-    client = Logix::Client.new
-    client.user_agent = "CustomLogixClient/0.0.1"
-    assert_equal "CustomLogixClient/0.0.1", client.user_agent
+    @client.user_agent = "CustomLogixClient/0.0.1"
+    assert_equal "CustomLogixClient/0.0.1", @client.user_agent
   end
 
 end
