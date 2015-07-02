79d22d5dbf5f8e784b80aa4bd482f6843bfe8940
Rodrigo Haenggi <rodrigo@codegestalt.com>
2015-07-02 16:11:30 +0200

    #endpoint? method

--- lib/logix/client.rb
+++ lib/logix/client.rb
@@ -2,9 +2,17 @@ require 'logix/version'
 module Logix
   class Client
 
-    attr_accessor :password, :certificate, :private_key
+    attr_accessor :password, :certificate, :private_key, :endpoint
     attr_writer :user_agent
 
+    # Public: Initialize new Client.
+    # options - The Hash options used to refine the selection (default: {}):
+    #           :password  - Your initial password.
+    #           :certificate  - String path to the certificate.crt.pem file.
+    #           :private_key - String path to the key.crt.pem file.
+    #           :endpoint - The FQDN of the banking API
+    #
+    # Returns a Logix::Client object
     def initialize(options = {})
       options.each do |key, value|
         instance_variable_set("@#{key}", value)
@@ -20,6 +28,11 @@ module Logix
     # @return [Boolean]
     def credentials?
       !!(password && certificate && private_key)
+    end
+
+    # @return [Boolean]
+    def endpoint?
+      !!(endpoint)
     end
 
   end
--- test/logix/test_client.rb
+++ test/logix/test_client.rb
@@ -4,20 +4,32 @@ describe Logix::Client do
 
   subject { Logix::Client.new }
 
-  describe "#user_agent" do
+  describe '#user_agent' do
     it 'defaults LogixRubyGem/version' do
       assert_equal "Logix/#{Logix::VERSION}", subject.user_agent
     end
   end
 
-  describe "#user_agent=" do
+  describe '#user_agent=' do
     it 'overwrites the User-Agent string' do
       subject.user_agent = "CustomLogixClient/0.0.1"
       assert_equal "CustomLogixClient/0.0.1", subject.user_agent
     end
   end
 
-  describe "#credentials?" do
+  describe '#endpoint?' do
+    it 'returns true if the endpoint is present' do
+      client = Logix::Client.new(endpoint: "tb.raiffeisendirect.ch")
+      assert_equal true, client.endpoint?
+    end
+
+    it 'returns false if the endpoint is missing' do
+      client = Logix::Client.new()
+      assert_equal false, client.endpoint?
+    end
+  end
+
+  describe '#credentials?' do
     it 'returns true if all credentials are present' do
       client = Logix::Client.new(password: 'y0l0', certificate: "./path/to/certificate.crt.pem", private_key: "./path/to/private.key.pem")
       assert_equal true, client.credentials?
