a0307cb27fa845c2ef80dabffbf1bae25066c184
Rodrigo Haenggi <rodrigo@codegestalt.com>
2015-07-02 14:55:06 +0200

    overwrite user agent

--- lib/logix/client.rb
+++ lib/logix/client.rb
@@ -2,6 +2,8 @@ require 'logix/version'
 module Logix
   class Client
 
+    attr_writer :user_agent
+
     # @return [String]
     def user_agent
       @user_agent ||= "Logix/#{Logix::VERSION}"
--- test/logix/test_client.rb
+++ test/logix/test_client.rb
@@ -7,4 +7,10 @@ class Logix::ClientTest < MiniTest::Test
     assert_equal "Logix/#{Logix::VERSION}", client.user_agent
   end
 
+  def test_overwrite_user_agent
+    client = Logix::Client.new
+    client.user_agent = "CustomLogixClient/0.0.1"
+    assert_equal "CustomLogixClient/0.0.1", client.user_agent
+  end
+
 end
