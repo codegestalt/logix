9ec21de933f229c6fb23d96f1193b455b2e3d9d7
Rodrigo Haenggi <rodrigo@codegestalt.com>
2015-07-03 11:11:02 +0200

    remove legacy env variables

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -5,20 +5,3 @@ require 'minitest/autorun'
 require 'webmock/minitest'
 
 WebMock.disable_net_connect!(allow_localhost: true)
-
-
-def test_fqdn
-  ENV.fetch 'LOGIX_FQDN', 'td.mybank.com'
-end
-
-def test_password
-  ENV.fetch 'LOGIX_PASSWORD', 'supersecret'
-end
-
-def test_certificate
-  ENV.fetch 'LOGIX_CERTIFICATE', '/tmp/fake_certificate_path.crt.pem'
-end
-
-def test_certificate
-  ENV.fetch 'LOGIX_PRIVATE_KEY', '/tmp/fake_certificate_path.key.pem'
-end
