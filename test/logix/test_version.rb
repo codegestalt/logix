433f55fbd89d741d6b18f6020b12c57bfe721fbf
Rodrigo Haenggi <rodrigo@codegestalt.com>
2019-02-26 11:34:32 +0100

    version bump

--- test/logix/test_version.rb
+++ test/logix/test_version.rb
@@ -3,7 +3,7 @@ require 'minitest_helper'
 describe Logix do
   describe "VERSION" do
     it 'returns the correct current version' do
-      assert_equal "0.0.1", Logix::VERSION
+      assert_equal "0.0.4", Logix::VERSION
     end
   end
 end
